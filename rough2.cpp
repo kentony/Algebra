a4f029dcec606e5cc22f771d2803d040ca654b4c
Anton <titov.ant.workmail@gmail.com>
2017-03-23 12:53:53 +0400

    Create rough2.cpp

---
+++ rough2.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <io.h>
+#include <fstream>
+#include <vector>
+#include <string>
+
+
+using namespace std;
+
+
+int gcd(int a, int b) {
+	while (b) {
+		a %= b;
+		swap(a, b);
+	}
+	return a;
+}
+
+int main(){
+	int m = 50;
+	string alph = "abcdefghijklmnopqrstuvwxyz1234567890!#$%&'()*+,-./";
+	string word, outword;
+	ifstream in;
+	ofstream out;
+	string input = "input.txt";
+	string output = "output.txt";
+
+	int code;
+	cout << "1 - code, 2 - decode\n";
+	cin >> code;
+
+	if(code == 1){
+
+		in.open(input);
+		out.open(output);
+		int n, k;
+		while (true) {
+			cout << "Enter N and K" << endl;
+			cin >> n >> k;
+			if (gcd(n, m) == 1) break;
+			cout << "Not correct: GCD(" << n << ", 50) != 1\nRe-";
+		}
+		int A, B;
+		while(!(in.eof())){
+			in >> word;
+			outword = "";
+			for(int i = 0; i < word.length(); i++){
+				for (int j = 0; j < alph.size(); j++){
+					if (word[i] == alph[j]){
+						A = j;
+						B = (A * n + k) % m;
+						outword += alph[B];
+						break;
+					}
+				}
+			}
+			out << outword << " ";
+		}
+	}
+	else{
+
+		in.open(output);
+		out.open(input);
+		int n, k;
+		while (true) {
+			cout << "Enter N and K" << endl;
+			cin >> n >> k;
+			if (gcd(n, m) == 1) break;
+			cout << "Not correct: GCD(" << n << ", 50) != 1\nRe-";
+		}
+		int x = 0;
+        while ((((n * x) % m) - 1) != 0)
+            x++;
+		int A, B;
+		while(!(in.eof())){
+			in >> word;
+			outword = "";
+			for(int i = 0; i < word.length(); i++){
+				for (int j = 0; j < alph.size(); j++){
+					if (word[i] == alph[j]){
+						B = j;
+						A = (((B - k + m) % m) * x) % m;
+						outword += alph[A];
+						break;
+					}
+				}
+			}
+			out << " " << outword;
+		}
+	}
+
+	
+
+	return 0;
+}
